a4828e93ee0ef0dfe139294a9095b442b7a6eb66
Jacob Henriksen <Henriksen.Jacob@gmail.com>
2022-11-09 21:23:47 +0100

    Added Encryption Page. Still needs a lot of work. FX make gen key functionality

---
+++ SecurePasswordMVC/Controllers/EncryptionController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecurePasswordMVC.Models;
+using System.Security.Cryptography;
+
+namespace SecurePasswordMVC.Controllers
+{
+    public class EncryptionController : Controller
+    {
+        public IActionResult Encryption()
+        {
+            var vm = new EncryptionModel();
+          
+            ViewBag.SavedText = EncryptionModel.savedText;
+            ViewBag.Timed = EncryptionModel.timed;
+            return View(vm);
+        }
+        public IActionResult EncryptDecrypt(EncryptionModel encryptionModel)
+        {
+            if (encryptionModel.PlainText == null && encryptionModel.CipherText == null)
+            {
+                return RedirectToAction("Encryption");
+            }
+            else
+            {
+                if (encryptionModel.UserChosenAlgorithm == "AES" || encryptionModel.UserChosenAlgorithm == null)
+                {
+                    encryptionModel.Algorithm = EncryptionModel.AES;
+                }
+                else
+                {
+                    encryptionModel.Algorithm = EncryptionModel.Triple_DES;
+                }
+                
+                //Date time and date span to see how long the process takes. 
+                DateTime timeStart = DateTime.Now;
+                
+                if (encryptionModel.PlainText != null)
+                {
+                    string returnedText = EncryptionModel.Encrypt(encryptionModel, encryptionModel.Algorithm, encryptionModel.PlainText);
+                    EncryptionModel.savedText = returnedText;
+                    Console.WriteLine(returnedText);
+                }
+                else if(encryptionModel.CipherText != null)
+                {
+                    string returnedText = EncryptionModel.Decrypt(encryptionModel, encryptionModel.Algorithm, encryptionModel.CipherText);
+                    EncryptionModel.savedText = returnedText;
+                    Console.WriteLine(returnedText);
+                }
+
+                DateTime timeEnd = DateTime.Now;
+                EncryptionModel.timed = timeEnd.Subtract(timeStart);
+                
+                return RedirectToAction("Encryption");
+            }
+        }
+
+        
+
+
+    }
+}
---
+++ SecurePasswordMVC/Models/EncryptionModel.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CodeActions;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SecurePasswordMVC.Models
+{
+    public class EncryptionModel
+    {
+        public static string? savedText;
+        public static TimeSpan? timed;
+
+        static public SymmetricAlgorithm Triple_DES = TripleDES.Create();
+        static public SymmetricAlgorithm AES = Aes.Create();
+
+        public List<SelectListItem> CipherAlgorithms = new()
+        { 
+            new SelectListItem { Text = "Triple_DES", Value = "Triple_DES" },
+            new SelectListItem { Text = "AES", Value = "AES" }
+        };
+
+        [Required]
+        public string Key { get; set; }
+        [Required]
+        public string IV { get; set; }
+        [Required]
+        public string UserChosenAlgorithm { get; set; }
+        public SymmetricAlgorithm? Algorithm { get; set; }
+        public string? PlainText { get; set; }
+        public string? CipherText { get; set; }
+        [Required]
+        public CipherMode Cipher { get; set; }
+        public PaddingMode Padding { get; set; }
+
+        public static string Encrypt(EncryptionModel encryptionModel, SymmetricAlgorithm algorithm, string text)
+        {
+            byte[] array;
+            
+            algorithm.Key = Encoding.UTF8.GetBytes(encryptionModel.Key);
+            algorithm.IV = Encoding.UTF8.GetBytes(encryptionModel.IV);
+            
+            ICryptoTransform encryptor = algorithm.CreateEncryptor(algorithm.Key, algorithm.IV);
+            
+            using(MemoryStream ms = new MemoryStream())
+            {
+                using(CryptoStream cs = new CryptoStream((Stream)ms, encryptor, CryptoStreamMode.Write))
+                {
+                    using(StreamWriter sw = new StreamWriter((Stream)cs))
+                    {
+                        sw.Write(text);
+                    }
+                    array = ms.ToArray();
+                }
+            }
+            return Convert.ToBase64String(array);
+        }
+        public static string Decrypt(EncryptionModel encryptionModel, SymmetricAlgorithm algorithm, string text)
+        {
+            byte[] buffer = Convert.FromBase64String(text);
+            
+            algorithm.Key = Encoding.UTF8.GetBytes(encryptionModel.Key);
+            algorithm.IV = Encoding.UTF8.GetBytes(encryptionModel.IV);
+
+            ICryptoTransform decryptor = algorithm.CreateDecryptor(algorithm.Key, algorithm.IV);
+
+            using (MemoryStream ms = new MemoryStream(buffer))
+            {
+                using (CryptoStream cs = new CryptoStream((Stream)ms, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader sr = new StreamReader((Stream)cs))
+                    {
+                        return sr.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
